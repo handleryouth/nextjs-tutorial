78e11348ab8025347755e596f44a526de75e7da8
Tony <rafaeltonydavid@yahoo.com>
2021-09-03 16:53:21 +0700

    fix: capitalization on import

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,15 @@
-import Link from "next/Link"
+import Link from "next/link";
 //as from next, image using this tag
-import Image from "next/image"
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <nav>
       <div className="logo">
-      {/* this thing is automatically lazy loading */}
-        <Image src="/logo.png" width={128} height={77} alt="logo"/>
+        {/* this thing is automatically lazy loading */}
+        <Image src="/logo.png" width={128} height={77} alt="logo" />
       </div>
-      <Link href="/" >Home</Link>
+      <Link href="/">Home</Link>
       <Link href="/about">About</Link>
       <Link href="/ninjas">Ninja Listing</Link>
     </nav>
